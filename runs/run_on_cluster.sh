60679a57503c2adf5293b2adc3aef531d798424b
Koshlan Mayer-Blackwell <46639063+kmayerb@users.noreply.github.com>
2020-03-17 16:19:29 -0700

    update to match structure

--- runs/run_on_cluster.sh
+++ runs/run_on_cluster.sh
@@ -1,8 +1,8 @@
 #! bin/bash
 ml nextflow 
 
-BATCHFILE=s3_test_manifest.csv
-NFCONFIG=aws_cluster.config
+BATCHFILE=manifests/s3_test_manifest.csv
+NFCONFIG=configs/aws_cluster.config
 PROJECT=aws2-test
 OUTPUT_FOLDER=s3:/fh-pi-kublin-j-microbiome/scratch-delete30/testpub
 WORK_DIR=s3:/fh-pi-kublin-j-microbiome/scratch-delete30/testwork
